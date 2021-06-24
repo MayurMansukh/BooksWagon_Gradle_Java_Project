4896d076b8c42a4d866d353d4b7be0f60db49b61
MayurMansukh <mansukhmayur99@gmail.com>
2021-06-24 15:17:19 +0530

    update project

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -7,26 +7,13 @@
 package test;
 
 import bridgelabz.base.Base;
-import bridgelabz.pages.LoginPage;
-import org.testng.Assert;
+
 import org.testng.annotations.Test;
 
 public class LoginTest extends Base {
 
     @Test
-    public void LoginTest() throws Exception {
-        LoginPage loginPage = new LoginPage(driver);
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.bookswagon.com/");
-        loginPage.clickLogin();
-        Thread.sleep(1000);
-        loginPage.enterUserName("mansukhmayur99@gmail.com");
-        Thread.sleep(1000);
-        loginPage.enterPassword("Password1@2");
-        Thread.sleep(1000);
-        loginPage.clickSubmitBtn();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.bookswagon.com/myaccount.aspx");
-        Thread.sleep(1000);
-        loginPage.clickLogoutopt();
-        loginPage.clickLogoutBtn();
+    public void LoginTest()  {
+        driver.get("https://www.bookswagon.com/");
     }
 }
